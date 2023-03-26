75fd3da3f410583f6b1f3aea6e244eea396d59c4
Neeraj Gangwar <gangwar2@illinois.edu>
2023-03-26 16:52:32 -0500

    Add functionality to move a span

--- scripts/add_noise_ner_spans.py
+++ scripts/add_noise_ner_spans.py
@@ -1,13 +1,16 @@
 import json
 import random
+import re
+import numpy as np
 from argparse import ArgumentParser
 
 
 class AddNERSpanNoise:
-    def __init__(self, dataset_file: str, out_file: str, prob: float = 0.15):
+    def __init__(self, dataset_file: str, out_file: str, prob: float = 0.15, max_span_width: int = 4):
         self.dataset_file = dataset_file
         self.out_file = out_file
         self.prob = prob
+        self.max_span_width = max_span_width
 
         all_labels = set()
         with open(self.dataset_file, "r") as f:
@@ -19,8 +22,38 @@ class AddNERSpanNoise:
         self.all_labels = list(all_labels)
 
 
+    def is_overlapping(self, span1, span2):
+        return span2[0] <= span1[1] <= span2[1] or span1[0] <= span2[1] <= span1[1]
+
+
+    def find_non_overlapping_spans(self, existing_spans, possible_spans):
+        non_overlapping_spans = []
+        for possible_span in possible_spans:
+            overlap = any([self.is_overlapping(possible_span, existing_span) for existing_span in existing_spans])
+            if not overlap:
+                non_overlapping_spans.append(possible_span)
+
+        return non_overlapping_spans
+
+
+    def find_possible_spans(self, document, spans):
+        existing_spans = [(span["start"], span["end"]) for span in spans]
+        space_indices = np.array([m.start() for m in re.finditer(r"\s", document) if document[m.start() - 1] != "." and document[m.end()] != "."])
+        start_indices = space_indices[:-1] + 1
+        end_indices = space_indices[1:]
+
+        possible_spans = []
+        for width in range(self.max_span_width):
+            possible_spans.extend(list(zip(start_indices[: -width], end_indices[width :])))
+
+        return self.find_non_overlapping_spans(existing_spans, possible_spans)
+
+
     def noisy_spans(self, document, spans):
         noisy_spans = []
+        possible_spans = self.find_possible_spans(document, spans)
+
+        used_spans = []
         for span in spans:
             rand = random.random()
             if rand > self.prob:
@@ -28,12 +61,17 @@ class AddNERSpanNoise:
                 continue
 
             # Drop the span in half the cases.
-            if rand <= self.prob / 2:
+            if rand <= self.prob / 3:
                 continue
-
             # Replace label in half the cases.
-            if rand > self.prob / 2:
+            elif rand > self.prob / 3 and rand <= 2 * self.prob / 3:
                 span["label"] = random.choice(self.all_labels)
+                noisy_spans.append(span)
+            # Move the span
+            else:
+                possible_span = random.choice(self.find_non_overlapping_spans(used_spans, possible_spans))
+                used_spans.append(possible_span)
+                span["start"], span["end"] = int(possible_span[0]), int(possible_span[1])
                 noisy_spans.append(span)
 
         return noisy_spans
