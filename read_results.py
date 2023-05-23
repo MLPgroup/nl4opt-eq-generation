b156482609b54dc178546b19f82776385a3c1f9b
Neeraj Gangwar <gangwar2@illinois.edu>
2023-05-23 14:00:51 -0500

    Add options to read results script

--- read_results.py
+++ read_results.py
@@ -5,17 +5,23 @@ import json
 import statistics as s
 
 
-def read_results(dirname, beam_size):
+def read_results(dirname, beam_size, is_copy, is_ner, is_per_declr):
     dirlist = glob.glob(f"output/{dirname}/*")
     score_dict = {}
     for dirpath in dirlist:
         result_file = f"{dirpath}/test_results_bs{beam_size}.json"
         ckpt = f"{dirpath}/best-checkpoint.mdl"
-        seed = torch.load(ckpt, map_location = torch.device("cpu"))["config"]["seed"]
+        config = torch.load(ckpt, map_location = torch.device("cpu"))["config"]
+        seed = config["seed"]
+
+        assert config["use_copy"] == is_copy
+        assert config["enrich_ner"] == is_ner
+        assert config["is_per_declr"] == is_per_declr
+
         with open(result_file) as f:
             score = f.read()
             score_dict[seed] = round(float(score), 3)
-    
+
     return score_dict
 
 
@@ -24,9 +30,16 @@ def main():
     parser.add_argument("--config", type = str, required = True)
     parser.add_argument("--beam_size", type = int, required = True)
     parser.add_argument("--n_expected_runs", type = int, required = True)
+    parser.add_argument("--no_copy", action = "store_true", default = False)
+    parser.add_argument("--no_ner", action = "store_true", default = False)
+    parser.add_argument("--no_per_declr", action = "store_true", default = False)
     args = parser.parse_args()
 
-    results = read_results(args.config, args.beam_size)
+    is_copy = not args.no_copy
+    is_ner = not args.is_ner
+    is_per_declr = not args.no_per_declr
+
+    results = read_results(args.config, args.beam_size, is_copy = is_copy, is_ner = is_ner, is_per_declr = is_per_declr)
     assert len(results) == args.n_expected_runs
 
     if len(results) > 1:
